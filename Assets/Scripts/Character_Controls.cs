24bcca88385ef14771f6a173f02276da48fd6f36
NegativeVibes <xavier_rate@outlook.com>
2021-04-05 21:05:37 +1000

    Push, Pull Mechanic done?
    The framework of the mechanic is there but it seems janky, I'll fine tune it when I find the time

--- Assets/Scripts/Character_Controls.cs
+++ Assets/Scripts/Character_Controls.cs
@@ -6,25 +6,20 @@ using UnityEngine;
 public class Character_Controls : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float move = 0f;
-    public float move2 = 0f;
-    public float speed = 10f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    public float moveSpeed = 5f;
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        move = Input.GetAxis("Vertical");
-        move2 = Input.GetAxis("Horizontal");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(move2 * speed, move * speed);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
+
+
---
+++ Assets/Scripts/PlayerBoxPull.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerBoxPull : MonoBehaviour
+{
+    public float defaultMass;
+    public float imovableMass;
+    public bool beingPushed;
+    float xPos;
+    float yPos;
+
+    public Vector3 lastPos;
+
+    public int mode;
+    public int colliding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xPos = transform.position.x;
+        yPos = transform.position.y;
+        lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (mode == 0)
+        {
+            if (beingPushed == false)
+            {
+                transform.position = new Vector3(xPos, transform.position.y);
+                transform.position = new Vector3(yPos, transform.position.x);
+            }
+            else
+                xPos = transform.position.x;
+                yPos = transform.position.y;
+        }
+        else if (mode == 1)
+        {
+
+            if (beingPushed == false)
+            {
+                GetComponent<Rigidbody2D>().mass = imovableMass;
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().mass = defaultMass;
+            }
+
+        }
+    }
+}
